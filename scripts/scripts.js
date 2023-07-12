29f22548b10ae4e8096ab6fc6919ebb7beb4d8f2
a.blonskyi <ablonskyi.developer@gmail.com>
2023-07-12 12:37:02 +0300

    homework 4 fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,24 +7,28 @@ let ageMessege;
 let i = age % 10;
 let j = age % 100;
 
-if (j >= 11 && j <= 19) {
-    ageMessege = `Ваш вік: ${age} років`;
-} else if (i == 1) {
-    ageMessege = `Ваш вік: ${age} рік`;
-} else if (i >= 2 && i <= 4) {
-    ageMessege = `Ваш вік: ${age} роки`;
-} else if (age == 0 || age >= 5 && age <= 20) {
-    ageMessege = `Ваш вік: ${age} років`;
-} else {
-    ageMessege = `Ваш вік: ${age} років`;
-}
-
-if (yearOfBirth === null) {
+if (yearOfBirth === null || yearOfBirth == '' || yearOfBirth == " " || isNaN(+yearOfBirth)) {
     ageMessege = 'Шкода, що Ви не захотіли ввести свій(ю) рік народження'
-}
-
-if (age > 100 && yearOfBirth !== null) {
-    ageMessege = `Вам більше ста років? Непогано!)`;
+} else {
+    if (age < 1) {
+        ageMessege = `Не може такого бути`; 
+    } else {
+        if (j >= 11 && j <= 19) {
+            ageMessege = `Ваш вік: ${age} років`;
+        } else if (i == 1) {
+            ageMessege = `Ваш вік: ${age} рік`;
+        } else if (i >= 2 && i <= 4) {
+            ageMessege = `Ваш вік: ${age} роки`;
+        } else if (age == 0 || age >= 5 && age <= 20) {
+            ageMessege = `Ваш вік: ${age} років`;
+        } else {
+            ageMessege = `Ваш вік: ${age} років`;
+        }
+        
+        if (age > 100 && yearOfBirth !== null) {
+            ageMessege = `Вам більше ста років? Непогано!)`;
+        }
+    }
 }
 
 if (city == 'Київ' || city == 'київ') {
@@ -35,28 +39,35 @@ if (city == 'Київ' || city == 'київ') {
     country = 'Великобританіі'
 }
 
-if (country == '' && city !== null) {
-    showLiveMessege = `Ти живеш у місті ${city}`;
-} else if (city === null) {
-    showLiveMessege = 'Шкода, що Ви не захотіли ввести своє місто';
+if (city === null || city == '' || city == ' ') {
+    showLiveMessege = 'Шкода, що Ви не захотіли ввести своє місто'
 } else {
-    showLiveMessege = `Ти живеш у столиці ${country}`
-} 
+    if (country == '') {
+        showLiveMessege = `Ти живеш у місті ${city}`;
+    } else {
+        showLiveMessege = `Ти живеш у столиці ${country}`
+    }
+}
+
+
 
 let sport = prompt('Твій улюбленний вид спорту?');
 let idol = '';
-let sportMessege = `Круто! Хочеш стати як ${idol}`;
+let sportMessege;
 
 if(sport === null || sport == '' || sport == ' ') {
     sportMessege = 'Шкода, що Ви не захотіли ввести свій улюбленний спорт'
 } else if (sport == 'футбол' || sport == 'Футбол') {
     idol = 'Мессі';
+    sportMessege = `Круто! Хочеш стати як ${idol}`
 } else if (sport == 'бокс' || sport == 'Бокс') {
     idol = 'Кличко';
+    sportMessege = `Круто! Хочеш стати як ${idol}`
 } else if (sport == 'баскетбол' || sport == 'Баскетбол') {
     idol = 'Джордан'
+    sportMessege = `Круто! Хочеш стати як ${idol}`
 } else {
-    sportMessege = 'Шкода, але я нічого не знаю про такий вид спорту'
+    sportMessege = `Круто, твій улюбленний вид спорту - ${sport}`
 }
 
 alert(`${ageMessege}\n${showLiveMessege}\n${sportMessege}`);
